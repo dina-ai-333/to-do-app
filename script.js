fd94ff2fc54eca0e4c9eca095a8db45f86aa9139
dina <evadinabedok@gmail.com>
2025-10-18 10:25:31 +0200

    Dodana funkcija za brisanje opravila

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function dodajOpravilo() {
   const li = document.createElement('li');
   li.textContent = task;
 
+  li.addEventListener('click', () => {
+    li.remove();
+  });
+
   document.getElementById('taskList').appendChild(li);
   input.value = '';
 }
